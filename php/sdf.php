f9da606b900f468a2147ef15c11d2bedf2db4cd4
testa <testa.a.tapos@gmail.com>
2013-06-27 18:06:36 +0100

    Add more google tag

--- php/sdf.php
+++ php/sdf.php
@@ -6,3 +6,14 @@ $botgoogle = strpos($_SERVER['HTTP_USER_AGENT'], $googlebottag);
 if ($botgoogle === True){
    exit('Error 500 Unwanted visitor!!!!!!');
 }
+
+$googlebottag   = '+https://developers.google.com/+/web/snippet/';
+$botgoogle = strpos($_SERVER['HTTP_USER_AGENT'], $googlebottag);
+
+
+if ($botgoogle === True){
+   exit('Error 500 Unwanted visitor!!!!!!');
+}
+
+
+?>
